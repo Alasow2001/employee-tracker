518a914d8c13e458153204e2151fae532d52ed28
Ahmed Alasow <a.alasow01@gmail.com>
2022-12-14 15:32:41 +0000

    Updated schema file

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DELETE DATABASE employee_db i
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id int NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+    FOREIGN KEY (role_id),
+    REFERENCES roleid)
+    ON DELETE SET NULL
+);
