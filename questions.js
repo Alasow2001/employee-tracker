f85f5c14c037269302fa8bb80c98314fa28b017a
Ahmed Alasow <a.alasow01@gmail.com>
2022-12-15 14:11:00 +0000

    Started adding variables to hold the questions for the employee, department and manager

---
+++ questions.js
@@ -0,0 +1,36 @@
+const MainQuestions = [
+    {
+        type: 'list',
+        name:'option',
+        message: 'What would you like to do?',
+        choices: ['Add a department', 'Add an employee', 'Add a role']
+    }
+]
+
+const AddDepartment = [
+    {
+        type: 'input',
+        name: 'department',
+        message: 'Enter a new department name:'
+    }
+]
+
+const AddRole = [
+    {
+        type: 'input',
+        name: 'role',
+        message: 'Enter a new role: '
+    }
+]
+
+const AddEmployee = [
+    {
+
+    }
+]
+
+const UpdateEmployee = [
+    {
+
+    }
+]
